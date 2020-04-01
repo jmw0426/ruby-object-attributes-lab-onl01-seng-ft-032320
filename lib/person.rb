54748df93664f45991faa0bfb8c4e46448b427e0
Joshua Williams <jmw0426@hotmail.com>
2020-04-01 19:43:05 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+ 
+  def name=(name) 
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+end
+beyonce = Person.new("Beyonce")
+  beyonce.name
+
+class Person
+  
+ def job=(job) 
+     @job = job
+ end
+ 
+ def job
+     @job
+ end
+end
+beyonce = Person.new("Singer")
+beyonce.job 
